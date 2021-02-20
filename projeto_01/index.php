81840279c763b78a395d07c19dac254241526d0e
Vitor Félix de Araujo <vitorfelix37@gmail.com>
2021-02-20 17:19:11 -0300

    finalizacao html e inicio do css

---
+++ projeto_01/index.php
@@ -0,0 +1,117 @@
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="keywords" content="palavras chave, do, meu, site"> 
+    <meta name="description" content="Descrição do meu site"> 
+    <title>Projeto 01</title>
+    <link href="estilo/all.min.css" rel="stylesheet"> <!--load all styles -->
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:300, 400, 700" rel="stylesheet">
+    <link rel="stylesheet" href="estilo/style.css" />
+    
+</head>
+<body>
+    <header>
+        <div class="center">
+            <div class="logo left">Logomarca</div><!--logo-->
+            <nav class="desktop right">
+                <ul>
+                    <li><a href="#">Home</li>
+                    <li><a href="#">Sobre</li>
+                    <li><a href="#">Serviços</li>
+                    <li><a href="#">Contato</li>
+                </ul>
+            </nav><!--Desktop-->
+            <nav class="mobile right">
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">Sobre</a></li>
+                    <li><a href="#">Serviços</a></li>
+                    <li><a href="#">Contato</a></li>
+                </ul>
+            </nav><!--Mobile-->
+            <div class="clear"></div>
+        </div><!--center-->
+    </header>
+
+    <section class="banner-principal">
+        <div class="center">
+            <form>
+                <h2>Qual o seu melhor e-mail?</h2>
+                <input type="email" name="email" required />
+                <input type="submit" name="acao" value="Cadastrar!">
+            </form>
+        </div><!--center-->
+    </section><!--banner principal-->
+    <section class="descricao-autor">
+        <div class="center">
+            <div class="w50 left">
+                <h2>Vitor F. Araujo</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer elementum enim non mauris ultricies gravida. Curabitur mollis libero a metus tincidunt ullamcorper. Nulla condimentum dolor ac nisl porta consequat id et massa. Phasellus in nisi urna. Maecenas id ante eros. Proin blandit iaculis metus. Pellentesque dui sem, volutpat sed turpis a, eleifend tempor metus.</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer elementum enim non mauris ultricies gravida. Curabitur mollis libero a metus tincidunt ullamcorper. Nulla condimentum dolor ac nisl porta consequat id et massa. Phasellus in nisi urna. Maecenas id ante eros. Proin blandit iaculis metus. Pellentesque dui sem, volutpat sed turpis a, eleifend tempor metus.</p>
+            </div><!--w50-->
+            <div class="w50 left">
+                <img id="autor" src="images/foto-autor.jpeg">
+            </div><!--w50-->
+            <div class="clear"></div>
+        </div><!--center--> 
+    </section><!--descricao-autor-->
+
+    <section class="especialidades">
+        <div class="center">
+            <h2 class="title">Especialidades</h2>
+            <div class="w33 left box-especialidade">
+                <h3><i class="fab fa-css3-alt"></i></h3>
+                <h3>CSS3</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer elementum enim non mauris ultricies gravida. </p>
+            </div><!--box-especialidade-->
+            <div class="w33 left box-especialidade">
+                <h3><i class="fab fa-html5"></i></h3>
+                <h3>HTML5</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer elementum enim non mauris ultricies gravida. </p>
+            </div><!--box-especialidade-->
+            <div class="w33 left box-especialidade">
+                <h3><i class="fab fa-js-square"></i></h3>
+                <h3>JavaScript</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer elementum enim non mauris ultricies gravida. </p>
+            </div><!--box-especialidade-->
+        </div><!--center-->
+    </section><!--Especialidades-->
+    <section class="extras">
+        <div class="center">
+            <div class="w50 left">
+                <h2 class="title">Depoimentos</h2>
+                <div class="depoimento-single">
+                    <p class="depoimento-descricao">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer elementum enim non mauris ultricies gravida. Curabitur mollis libero a metus tincidunt ullamcorper. Nulla condimentum dolor ac nisl porta consequat id et massa. Phasellus in nisi urna. Maecenas id ante eros. Proin blandit iaculis metus.</p>
+                    <p class="nome-autor">Lorem Ipsum</p>
+                </div><!--depoimento-single-->
+                <div class="depoimento-single">
+                    <p class="depoimento-descricao">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer elementum enim non mauris ultricies gravida. Curabitur mollis libero a metus tincidunt ullamcorper. Nulla condimentum dolor ac nisl porta consequat id et massa. Phasellus in nisi urna. Maecenas id ante eros. Proin blandit iaculis metus.</p>
+                    <p class="nome-autor">Lorem Ipsum</p>
+                </div><!--depoimento-single-->
+                <div class="depoimento-single">
+                    <p class="depoimento-descricao">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer elementum enim non mauris ultricies gravida. Curabitur mollis libero a metus tincidunt ullamcorper. Nulla condimentum dolor ac nisl porta consequat id et massa. Phasellus in nisi urna. Maecenas id ante eros. Proin blandit iaculis metus.</p>
+                    <p class="nome-autor">Lorem Ipsum</p>
+                </div><!--depoimento-single-->
+            </div><!--w50-->
+            <div class="clear"></div>
+            <div class="w50 left">
+                <h2 class="title">Serviços</h2>
+                <div class="servicos">
+                    <ul>
+                        <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer elementum enim non mauris ultricies gravida. Curabitur mollis libero a metus tincidunt ullamcorper. Nulla condimentum dolor ac nisl porta consequat id et massa. Phasellus in nisi urna. Maecenas id ante eros. Proin blandit iaculis metus.</li>
+                        <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer elementum enim non mauris ultricies gravida. Curabitur mollis libero a metus tincidunt ullamcorper. Nulla condimentum dolor ac nisl porta consequat id et massa. Phasellus in nisi urna. Maecenas id ante eros. Proin blandit iaculis metus.</li>
+                        <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer elementum enim non mauris ultricies gravida. Curabitur mollis libero a metus tincidunt ullamcorper. Nulla condimentum dolor ac nisl porta consequat id et massa. Phasellus in nisi urna. Maecenas id ante eros. Proin blandit iaculis metus.</li>
+                    </ul>
+                </div><!--serviços-->
+            </div><!--w50-->
+            <div class="clear"></div>
+        </div><!--center-->
+    </section><!--extras-->
+    <footer>
+        <div class="center">
+            <p>Todos os direitos reservados</p>
+        </div><!--center-->
+    </footer>
+</body>
+</html>
